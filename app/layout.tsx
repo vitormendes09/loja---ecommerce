bb225b7a5244cf99d336f11cd1636d78b94909db
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2026-01-22 13:47:24 -0300

    fix: adicionando component NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-      </body>
+        <Navbar />
+        <main className="bg-slate-700 h-screen p-16">
+          {children}
+        </main>
+    
+      </body>    
     </html>
   );
 }
