1600cbcc949c0995cb559355643d1799e0b7b198
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2026-01-23 15:53:20 -0300

    listagem de produtos

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>
-        Hello World!
-      </h1>
+      
+      <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+
+
+        </div>
+
+
+
+      </div>
     </main>
   );
 }
